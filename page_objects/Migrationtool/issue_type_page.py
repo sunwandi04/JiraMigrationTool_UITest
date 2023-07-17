24e164c358ee0ca2595f9f129dc024e02f870fe8
luhuan-7 <419374961@qq.com>
2023-07-17 19:09:17 +0800

    -

--- page_objects/Migrationtool/issue_type_page.py
+++ page_objects/Migrationtool/issue_type_page.py
@@ -1,4 +1,5 @@
 from page_objects.Migrationtool.backup_page import BackupPage
+import re
 
 class IssueTypePage(BackupPage):
     def search_input(self, search_key):
@@ -17,7 +18,9 @@ class IssueTypePage(BackupPage):
         self.click_by_button("下一步")
 
     def goto_issue_type_page(self):
-        self.start_analyze('0712集成测试.zip','0712集成测试.zip')
+        self.page.locator("div").filter(has_text=re.compile(r"^请选择$")).nth(4).click()
+        self.start_analyze('0712集成测试.zip', '0712集成测试.zip')
+        self.page.get_by_text("解析完成").click()
         self.click_by_button("下一步")
         self.choose_ones_org()
         self.choose_jira_proj()
